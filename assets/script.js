c13fafe2418be3f71db790830b12afca86f3def1
Barchen <student@bcdm52558.local>
2016-11-04 10:28:59 -0400

    eh?

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,6 @@ $( document ).ready(function() {
 	    $('h3').css('background-color', 'yellow');
 	  }
 	);
-	$('a').click(
-	  function() {
-	    $('h3').css('display', 'none');
-	  }
-	);
-	$('body').mouseenter(
-	  function() {
-	    $('body').css('position', 'relative');
-	    $('body').css('left', '30px');
-	  }
-	);
 });
 
 
