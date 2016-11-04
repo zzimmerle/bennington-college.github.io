273b9d3777c4db8af4ee1ec7a1f1b7fda2067cfc
bennington-college <home@homes-MacBook-Pro.local>
2016-11-04 10:21:31 -0400

    add exercise suggestions to script.js

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,11 @@ $( document ).ready(function() {
 });
 
 
-
+// exercises:
+// when you .hover() over 'h3' elements, change 'background-color' to 'yellow'
+//
+// when you click on an 'a' element, make it disappear: change 'display' to 'none'
+//
+// when you move the mouse over the 'body' element, shift it to the right:
+//		change 'position' to 'relative' and 'left' to '30px'
+//
