ed57a6e576272ea90893ab5e4dbb98e9384afc16
Barchen <student@bcdm52558.local>
2016-12-02 12:00:53 -0500

    add zzc

--- assets/plaintext_textarea.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function () {
-    $('#jqxTextArea').jqxTextArea({ width: 250, height: 100, placeHolder: 'Enter text to be frequency counted here' });
-});
-
-var originalPlaintext = $('#jqxTextArea').val();
